6dc3360d276b35a001f879015bcb73fd1b8cc1ab
IvoneLopez2407 <iglopez@ucundinamarca.edu.co>
2023-03-17 18:18:42 -0500

    PROGRAMA
    PROGRAMA

---
+++ src/main/java/com/mycompany/calculadora2/InterfazCalculadora.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.calculadora2;
+
+/**
+ *
+ * @author ivone
+ */
+public interface InterfazCalculadora {
+    void solicitarNumeros();
+    int sumarEnteros();
+    int restarEnteros(/*int numero1, int numero2*/);
+    int multiplicarEnteros();
+    int dividirEnteros();
+    double calcularRaizCuadrada();    
+}
+
